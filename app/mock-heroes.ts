32058a53601029ea38b6c8e2bab098b540af6a7b
Brandon Parmley <orem.bparmley@gmail.com>
2016-03-21 20:00:37 -0600

    added routing, split the HeroComponent from the AppComponent Shared HeroService across components

--- app/mock-heroes.ts
+++ app/mock-heroes.ts
@@ -1,13 +1,14 @@
-import {Hero} from './hero';
+import { Hero } from './hero';
+
 export var HEROES: Hero[] = [
-    {"id": 11, "name": "Mr. Nice"},
-    {"id": 12, "name": "Narco"},
-    {"id": 13, "name": "Bombasto"},
-    {"id": 14, "name": "Celeritas"},
-    {"id": 15, "name": "Magneta"},
-    {"id": 16, "name": "RubberMan"},
-    {"id": 17, "name": "Dynama"},
-    {"id": 18, "name": "Dr IQ"},
-    {"id": 19, "name": "Magma"},
-    {"id": 20, "name": "Tornado"}
+	{"id": 11, "name": "Mr. Nice"},
+	{"id": 12, "name": "Surinam"},
+	{"id": 13, "name": "Draig"},
+	{"id": 14, "name": "Calgon"},
+	{"id": 15, "name": "Kolant"},
+	{"id": 16, "name": "RubberMan"},
+	{"id": 17, "name": "Dynama"},
+	{"id": 18, "name": "Dr IQ"},
+	{"id": 19, "name": "Magma"},
+	{"id": 20, "name": "Tornado"}
 ];
